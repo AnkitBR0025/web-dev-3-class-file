0da5dd0bceb4788985d3d6f5e6eb3355ab02e159
Ankit <ankitpandey12348409@gmail.com>
2026-09-08 15:16:41 +0530

    update file

--- lecture-16-middleware/server.js
+++ lecture-16-middleware/server.js
@@ -1,12 +1,17 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 const port = 3000;
 
-const logMiddleware = (req, res, next) => {
-    console.log("Request Method: ", req.method);
-    console.log("Request URL: ", req.url);
-    next();
-}
+// const logMiddleware = (req, res, next) => {
+//     console.log("\nRequest Method:", req.method,"Request URL:", req.url,"Date:", new Date().toLocaleString());
+//     next();
+// }
+////jo logMiddleware hai usko humne morgan ke through replace kar diya hai, morgan ek popular middleware hai jo ki request aur response ke details ko log karta hai.
+app.use(morgan());  ////global middleware
+// app.use(morgan("dev"));  ////global middleware
+// app.use(morgan("tiny"));  ////global middleware
+// app.use(morgan("combined"));  ////global middleware
 
 const apiCheckMiddleware=(req,res,next)=>{
     if(req.query.API_KEY=="1234"){
@@ -19,7 +24,7 @@ const apiCheckMiddleware=(req,res,next)=>{
     }
 }
 
-// app.use(logMiddleware);                ////global middleware
+//app.use(logMiddleware);                ////global middleware
 // app.use(apiCheckMiddleware);
 
 app.get("/", (req, res) => {
@@ -32,7 +37,7 @@ app.get("/", (req, res) => {
 // })
 
 //specific middleware
-app.get("/students", logMiddleware, apiCheckMiddleware, (req, res) => {
+app.get("/students", apiCheckMiddleware, (req, res) => {
     console.log("Hello students");
     res.send("Hello students")
 })
